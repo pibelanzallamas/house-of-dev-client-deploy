b5a09f0c4b5ddd70c216f652fc533f54b659fe3b
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-06-25 19:45:42 -0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppRoutes from "./components/AppRoutes";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles/App.css";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <AppRoutes />
     </div>
   );
