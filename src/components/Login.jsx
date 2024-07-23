89d71c7334191726a9d2bcae6cc9ada9a7eec960
Brandon <brandoncastillo.09@gmail.com>
2024-07-23 15:03:25 -0300

    localstorage console

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,23 +4,14 @@ import { useDispatch } from "react-redux";
 import { setUser } from "../state/userState";
 import { useNavigate, Link } from "react-router-dom";
 import { alerts } from "../utils/alerts";
-// import { useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 function Login() {
   const email = useInput("");
   const password = useInput("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const [user, setUser] = useState(window.localStorage.getItem("text"));
-
-  // function setLocalStorage(value) {
-  //   try {
-  //     setUser(value);
-  //     window.localStorage.setItem("text", user);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
+  const [localStorageUser, setLocalStorageUser] = useLocalStorage("user", "");
 
   function handleLogin(e) {
     e.preventDefault();
@@ -37,16 +28,15 @@ function Login() {
       .then((payload) => {
         alerts("Aloha!", `Welcome ${payload.data.name} 🏝`, "success");
         dispatch(setUser(payload.data));
-        console.log(payload);
+        setLocalStorageUser(payload.data);
         navigate("/home");
-        // setLocalStorage(payload.data);
       })
       .catch((err) => {
         console.log(err);
         alerts("Nope!", "Email o password incorrectos ☠️", "danger");
       });
   }
-
+  console.log(localStorageUser);
   return (
     <div className="containerLogin">
       <div className="sideALogin">
