677c25d20769af9f4927abd5ea14f97c2750c968
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-08-05 09:16:56 -0300

    useLocalStorage off

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,23 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export function useLocalStorage(key, initialValue) {
-  const [storeValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      return initialValue;
-    }
-  });
+// export function useLocalStorage(key, initialValue) {
+//   const [storeValue, setStoredValue] = useState(() => {
+//     try {
+//       const item = window.localStorage.getItem(key);
+//       return item ? JSON.parse(item) : initialValue;
+//     } catch (error) {
+//       return initialValue;
+//     }
+//   });
 
-  const setValue = (value) => {
-    try {
-      setStoredValue(value);
-      window.localStorage.setItem("text", user);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//   const setValue = (value) => {
+//     try {
+//       setStoredValue(value);
+//       window.localStorage.setItem("text", user);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  return [storeValue, setValue];
-}
+//   return [storeValue, setValue];
+// }
